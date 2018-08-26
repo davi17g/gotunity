b0f3756869097b25aa07b37e9932cd4fbb82a204
David Gerchikov <davi17g@gmail.com>
2018-08-26 21:32:45 +0300

    added unittests

--- server_test.go
+++ server_test.go
@@ -3,23 +3,70 @@ package main
 import (
 	"testing"
 	"fmt"
+	"net"
+	"strconv"
+	"time"
 )
 
 func TestStartServer(t *testing.T) {
 	host := "127.0.0.1"
-	pin = 8888
-	pout = 8089
-	StartServer(host, pin, pout)
-	fmt.Println("Here")
+	pin := 8888
+	pout := 8089
+	ans := make(chan string)
+	msg := "TEST\r\n"
+	go StartServer(&host, &pin, &pout)
+	go SendUDPMessage(&host, &pin, &msg)
+	go ReciveUDPmessage(&host, &pout, ans)
+	select {
+	case result := <-ans:
+		if result == "TEST" {
+			fmt.Println("Test Succeded")
+			} else {
+				t.Fail()
+				break
+			}
+			case <- time.After(7 * time.Second):
+				fmt.Println("Test timouted")
+		}
+	}
+
+func SendUDPMessage(host *string, port *int, msg *string) {
+	packet := make([]byte, 1024)
+	address := *host + ":" + strconv.Itoa(*port)
+	conn, err := net.Dial("udp", address)
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		return
+	}
+	fmt.Fprintf(conn, *msg)
+	conn.Write(packet)
+	if err != nil {
+		fmt.Printf("Some error %v\n", err)
+
+	}
+	conn.Close()
 
 }
-
-func SendUDPMessage(host *string, port *int) {
-	addr := net.UDPAddr{Port: *port, IP: net.ParseIP(*host)}
-	packet :=  make([]byte, 1024)
-	address := *host + ":" + string(port)
-	fmt.Println(address)
-	conn, err := net.Dial("udp", "127.0.0.1:1234")
-
+func ReciveUDPmessage(host *string, port *int, comm chan <- string){
+	connect := "CONNECT\r\n"
+	packet := make([]byte, 1024)
+	p := make([]byte, 1024)
+	address := *host + ":" + strconv.Itoa(*port)
+	conn, err := net.Dial("udp", address)
+	if err != nil {
+		fmt.Printf("Some error %v", err)
+		return
+	}
+	fmt.Fprintf(conn, connect)
+	conn.Write(packet)
+	if err != nil {
+		fmt.Printf("Some error %v\n", err)
+	}
+	_ ,err = conn.Read(p)
+	if err != nil {
+		fmt.Printf("Some error %v\n", err)
+	}
+	result := string(p[0:4])
+	comm <- result
 
 }
